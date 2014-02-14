9091cbd9a734bcdeb2bd8ef2cbc44eedc6f07d11
xiaophi <philippe.castelli@gmail.com>
2014-02-14 15:28:03 +0100

    Définition des routes de l'application

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 var App = Ember.Application.create();
+
+App.Router.map(function () {
+    this.resource('catalog', function () {
+        this.resource('category', { path: '/category/:category_id' }, function () {
+            this.resource('item', { path: '/item/:item_id' });
+        });
+    });
+    this.route('checkout');
+});
+
+App.IndexRoute = Ember.Route.extend({
+    redirect: function () {
+        this.transitionTo('catalog');
+    }
+});
